45d3535eaa2fe5de03c2797e95a8dbd7eba57828
thenicopixie <391@holbertonschool.com>
2019-05-13 14:59:20 -0700

    design a max stack that supports push, pop, top, peekMax, and popMax

---
+++ Python/maxStack.py
@@ -0,0 +1,41 @@
+"""
+Design a max stack that supports push, pop, top, peekMax and popMax.
+
+push(x) -- Push element x onto stack.
+pop() -- Remove the element on top of the stack and return it.
+top() -- Get the element on the top.
+peekMax() -- Retrieve the maximum element in the stack.
+popMax() -- Retrieve the maximum element in the stack, and remove it. If you find more than one maximum elements, only remove the top-most one.
+"""
+class MaxStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        return self.stack.pop() 
+        
+    def top(self) -> int:
+        return self.stack[len(self.stack)-1]
+
+    def peekMax(self) -> int:
+        return max(self.stack)
+
+    def popMax(self) -> int:
+        c = (len(self.stack)-1) - (self.stack[::-1].index(max(self.stack)))
+        return self.stack.pop(c)
+
+
+# MaxStack object will be instantiated and called as such:
+# obj = MaxStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.peekMax()
+# param_5 = obj.popMax()
